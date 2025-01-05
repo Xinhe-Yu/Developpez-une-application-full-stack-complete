e9aa9751bd8ffa6049013039ae0ce25eb4a59b28
Xinhe YU <aspasia.yu@gmail.com>
2025-01-05 11:58:38 +0100

    stage

--- front/src/app/pages/home/home.component.ts
+++ front/src/app/pages/home/home.component.ts
@@ -1,16 +1,28 @@
+import { AsyncPipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
+import { Observable } from 'rxjs';
+import { SessionService } from 'src/app/services/session.service';
 @Component({
   selector: 'app-home',
   imports: [
     MatButtonModule,
-    RouterLink
+    RouterLink,
+    AsyncPipe
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  public $isLogged!: Observable<boolean>;
+
+  constructor(
+    private sessionService: SessionService,
+    private router: Router
+  ) {
+    this.$isLogged = this.sessionService.$isLogged();
+  }
 
 }
 // export class HomeComponent implements OnInit {
